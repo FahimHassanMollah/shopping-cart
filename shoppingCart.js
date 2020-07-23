6b52aaf6288335fff48ec252513923fc5893d133
FahimHassanMollah <fahimhassan0197@gmail.com>
2020-07-23 21:06:14 +0600

    sub total and total functionality added and remove item functionality added

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,3 +1,5 @@
+let mobile=true;
+let casing=true;
 document.getElementById('add-button-mobile')
     .addEventListener('click', event => {
 
@@ -32,6 +34,8 @@ function productCount(mobileCountId,num,mobilePriceId,singlePrice) {
         {
             document.getElementById(mobileCountId).value=mobileCountValue;
             document.getElementById(mobilePriceId).innerText=(mobileCountValue*singlePrice).toString();
+            totalprice();
+
 
         }
 
@@ -41,8 +45,60 @@ function productCount(mobileCountId,num,mobilePriceId,singlePrice) {
         {
             document.getElementById(mobileCountId).value=mobileCountValue;
             document.getElementById(mobilePriceId).innerText=(mobileCountValue*singlePrice).toString();
+            totalprice();
         }
 
     }
+
+
+
+}
+document.getElementById('mobile-item')
+    .addEventListener('click', event => {
+    document.getElementById('item-one').style.display="none";
+    mobile=false;
+    totalprice();
+
+});
+document.getElementById('casing-item')
+    .addEventListener('click', event => {
+        document.getElementById('item-two').style.display="none";
+        casing=false;
+        totalprice();
+
+    });
+
+
+
+function totalprice() {
+    document.getElementById('subtotal').innerText=null;
+  // let subtotalString=  document.getElementById('subtotal').innerText;
+  // let subtotalvalue= parseFloat(subtotal);
+  let casingString=document.getElementById('silicone-case-price').innerText;
+  let casingVlue=parseFloat(casingString);
+  let mobileString=document.getElementById('mobile-price').innerText;
+  let mobileValue=parseFloat(mobileString);
+  if (mobile===false)
+  {
+      mobileValue=0;
+
+  }
+  if (casing===false)
+  {
+      casingVlue=0;
+  }
+    let subtotal=mobileValue+casingVlue;
+
+    document.getElementById('subtotal').innerText=subtotal;
+    document.getElementById('total').innerText=subtotal;
+
+
+  // let taxString=  document.getElementById('tax').innerText;
+  // let taxValue=  parseFloat(tax);
+  //
+  // let totalString=  document.getElementById('total').innerText;
+  // let totalValue= parseFloat(total);
+
+
 
 }
